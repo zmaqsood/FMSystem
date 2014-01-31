cf0b3b0f8ece2e23d55bb0f36cd2335ee45bda57
zmaqsood <zmaqsood@luc.edu>
2014-01-31 00:30:02 -0600

    first commit

---
+++ src/main/java/FacilityMgmtSystem/FMS/App.java
@@ -0,0 +1,13 @@
+package FacilityMgmtSystem.FMS;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
