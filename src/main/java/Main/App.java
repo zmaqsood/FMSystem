6508792018fd3634a22d3552658e2bbd49361bad
zmaqsood <zmaqsood@luc.edu>
2014-02-02 16:11:51 -0600

    restructed packages

--- src/main/java/Main/App.java
+++ src/main/java/Main/App.java
@@ -1,9 +1,11 @@
-package Person;
+package Main;
 
 /**
  * Zain Maqsood
  *
  */
+
+//corresponds to the main class(s) for creating and calling different objects.
 public class App 
 {
     public static void main( String[] args )
